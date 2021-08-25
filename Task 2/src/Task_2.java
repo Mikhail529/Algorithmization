53459f30c424df7a3fe60d8660869f510ceb5eed
Mikhail Kireyenka <mixail.55290@gmail.com>
2021-08-26 00:10:08 +0300

    Add file task-2

---
+++ Task 2/src/Task_2.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CLS {
+    public static void main(String[] args) {
+
+        // Task 1. Дана последовательность действительных чисел a1,a2,...,an.
+        // Заменить все её члены, больше данного Z, этим числом. Посчитать количество замен
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter value Z: ");
+        int z = scanner.nextInt();
+        int k = 0;
+
+        for (int i=0; i<array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(array));
+
+        for (int i=0; i<array.length; i++) {
+            if (array[i] >= z) {
+                array[i] = z;
+                ++k;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Number of replacements: " + k);
+    }
+}
