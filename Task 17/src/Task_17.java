5eba5ba686d390ae001d4f54ea153aa75d9e30ba
Mikhail Kireyenka <mixail.55290@gmail.com>
2021-08-27 20:05:39 +0300

    Add file task-17

--- Task 17/src/Task_17.java
+++ Task 17/src/Task_17.java
@@ -4,12 +4,15 @@ public class CLS {
     public static void main(String[] args) {
 
         // Task 17. Сформировать квадратную матрицу порядка N по правилу A[I,J] = sin((I^2 - J^2) / N)
+        // и подсчитать количество положительных элементов в ней
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the square matrix with even number N: ");
         int n = scanner.nextInt();
 
         double[][] matrix = new double[n][n];
+
+        int k = 0;
 
         for (int i=0; i<matrix.length; i++) {
             for (int j=0; j<matrix.length; j++) {
@@ -20,6 +23,9 @@ public class CLS {
         for (int i=0; i<matrix.length; i++) {
             for (int j=0; j<matrix.length; j++) {
                 matrix[i][j] = Math.sin((Math.pow(i, 2) - Math.pow(j, 2)) / n);
+                if (matrix[i][j] > 0) {
+                    k++;
+                }
             }
         }
 
@@ -29,5 +35,6 @@ public class CLS {
             }
             System.out.println();
         }
+        System.out.println("Number of positive elements: " + k);
     }
 }
